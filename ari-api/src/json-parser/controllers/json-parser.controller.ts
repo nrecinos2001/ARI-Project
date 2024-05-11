621fb97eae04737e0244e62bdca58e17a451d9cc
Nestor Recinos <00083120@UCA.EDU.SV>
2024-05-11 08:57:35 -0600

    :construction: Add dto and initialize controller

--- ari-api/src/json-parser/controllers/json-parser.controller.ts
+++ ari-api/src/json-parser/controllers/json-parser.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('json-parser')
-export class JsonParserController {}
+export class JsonParserController {
+  @Post()
+  async parseText() {
+    
+  }
+}
