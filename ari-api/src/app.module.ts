e144b2d787151480822ba421ec2317d6e336ffc6
Nestor Recinos <00083120@UCA.EDU.SV>
2024-05-10 23:24:57 -0600

    :construction: Initialize json-parser module

--- ari-api/src/app.module.ts
+++ ari-api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JsonParserModule } from './json-parser/json-parser.module';
 
 @Module({
-  imports: [],
+  imports: [JsonParserModule],
   controllers: [AppController],
   providers: [AppService],
 })
