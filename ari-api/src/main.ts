22fb483fd6a9b474dd57767fe258464a378152fb
Nestor Recinos <00083120@UCA.EDU.SV>
2024-05-11 23:25:05 -0600

    :wrench: Setup Swagger

--- ari-api/src/main.ts
+++ ari-api/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
+import { setupSwagger } from '@Config/swagger';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
