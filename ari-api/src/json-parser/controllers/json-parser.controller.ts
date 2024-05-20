46a10e7a1d23034b70ceb2cd79668674ac5f7017
Nestor Recinos <00083120@UCA.EDU.SV>
2024-05-19 22:45:03 -0600

    :construction: Add method for controller

--- ari-api/src/json-parser/controllers/json-parser.controller.ts
+++ ari-api/src/json-parser/controllers/json-parser.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { ParseTextDto } from '../dto';
+import { JsonParserService } from '../services';
 
 @Controller('json-parser')
 export class JsonParserController {
+  constructor(private readonly jsonParserService: JsonParserService) {}
   @Post()
-  async parseText() {}
+  async parseText(@Body() parseTextDto: ParseTextDto) {
+    const response = this.jsonParserService.parse(parseTextDto);
+    return response;
+  }
 }
