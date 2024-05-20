c6db99cd30a4e5df59acd063beef4a61ba7300e1
Nestor Recinos <00083120@UCA.EDU.SV>
2024-05-19 22:49:32 -0600

    :construction: Format body for response

--- ari-api/src/json-parser/services/json-parser.service.ts
+++ ari-api/src/json-parser/services/json-parser.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+import { ParseTextDto } from '../dto';
+import { Separator } from '@JsonParser/enums';
+
 @Injectable()
-export class JsonParserService {}
+export class JsonParserService {
+  parse(parseTextDto: ParseTextDto) {
+    const { separator, text } = parseTextDto;
+    const splitter = separator === Separator.COMMA ? ',' : ';';
+    const textObject = text.reduce((acc, current) => {
+      const polygonInitIndex = current.indexOf('(');
+      const polygon = !polygonInitIndex
+        ? undefined
+        : current.slice(polygonInitIndex);
+      const splittedText = current.split(splitter);
+      const itemBody = {
+        documento: splittedText[0],
+        nombres: splittedText[1],
+        apellidos: splittedText[2],
+        tarjeta: splittedText[3],
+      };
+      acc.push(itemBody);
+      return acc;
+    }, []);
+    return textObject;
+  }
+}
