32154bcff469a8e9a59b255997a649eac5dc7b0e
Nestor Recinos <00083120@UCA.EDU.SV>
2024-06-08 21:23:34 -0600

    :art: lint code

--- ari-api/src/main.ts
+++ ari-api/src/main.ts
@@ -6,10 +6,10 @@ import { setupSwagger } from '@Config/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create( AppModule );
+  const app = await NestFactory.create(AppModule);
   app.enableCors(); // Habilitar CORS
-  app.useGlobalPipes( new ValidationPipe() );
-  setupSwagger( app );
-  await app.listen( 3000 );
+  app.useGlobalPipes(new ValidationPipe());
+  setupSwagger(app);
+  await app.listen(3000);
 }
 bootstrap();
