c474d4d92f35444600358526e699fca9a0d22193
Nestor Recinos <00083120@UCA.EDU.SV>
2024-06-09 23:43:17 -0600

    :sparkles: Encrypt and decrypt data in api call

--- ari-app/src/api/jsonParser.ts
+++ ari-app/src/api/jsonParser.ts
@@ -1,16 +1,22 @@
 import axios from 'axios';
 import { baseUrl } from '@/utils/baseUrl';
+import { decryptData, encryptData } from '@/app/utils';
+
+const key = 'key';
 
 export default async function sendString(data: string) {
     const requestBody = {
         text: [data],
         separator: "comma",
-        key: "key"
+        key,
     };
 
     try {
-        const response = await axios.post(`${baseUrl}/json-parser`, requestBody);
-        return response.data;
+        const encryptedDataToSend = encryptData(data, key);
+        requestBody.text = encryptedDataToSend;
+        const response = await axios.post<string[]>(`${baseUrl}/json-parser`, requestBody);
+        const decryptedResponse = decryptData(response.data[0], response.data[1], key);
+        return JSON.parse(JSON.parse(decryptedResponse));
     } catch (error) {
         console.error(error);
         throw error;
