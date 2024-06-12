38dab9e7c4f99685597976bee34964d212cd9c4a
Irvin Pineda <00024420@uca.edu.sv>
2024-06-12 06:03:26 -0600

    Add environment variables

--- ari-app/src/api/jsonParser.ts
+++ ari-app/src/api/jsonParser.ts
@@ -1,7 +1,9 @@
+// /api/jsonParser.ts
 import axios from 'axios';
 import { baseUrl } from '@/utils/baseUrl';
 import { decryptData, encryptData } from '@/app/utils';
 import { baseKey as key } from '@/utils/key';
+
 export default async function sendString( data: string, separator: string ) {
     const requestBody = {
         text: [data],
@@ -10,13 +12,13 @@ export default async function sendString( data: string, separator: string ) {
     };
 
     try {
-        const encryptedDataToSend = encryptData(data, key);
+        const encryptedDataToSend = encryptData( data, key );
         requestBody.text = encryptedDataToSend;
-        const response = await axios.post<string[]>(`${baseUrl}/json-parser`, requestBody);
-        const decryptedResponse = decryptData(response.data[0], response.data[1], key);
-        return JSON.parse(JSON.parse(decryptedResponse));
-    } catch (error) {
-        console.error(error);
+        const response = await axios.post<string[]>( `${baseUrl}/json-parser`, requestBody );
+        const decryptedResponse = decryptData( response.data[0], response.data[1], key );
+        return JSON.parse( JSON.parse( decryptedResponse ) );
+    } catch ( error ) {
+        console.error( error );
         throw error;
     }
 }
--- ari-app/src/utils/key.tsx
+++ ari-app/src/utils/key.tsx
@@ -1,1 +1,1 @@
-export const baseKey = 'key';
+export const baseKey: string = process.env.NEXT_PUBLIC_BASE_KEY as string;
